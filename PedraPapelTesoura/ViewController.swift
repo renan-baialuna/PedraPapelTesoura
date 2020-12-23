1dee7ec96f1b3a07c744844b0a34e2d6f1c53c21
Renan <renan.baialuna@gmail.com>
2020-12-23 16:36:26 -0300

    Initial Commit

---
+++ PedraPapelTesoura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PedraPapelTesoura
+//
+//  Created by Renan Baialuna on 23/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
